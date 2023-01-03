f232922fdf217e58b01d05f81bcb188244e5993a
Olishevsky Alex <olishevskyalex@gmail.com>
2023-01-03 13:10:58 +0400

    Clean code

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, {FC} from "react";
+
+import Button from "./components/Button";
+
+const App: FC = () => {
+  return (
+    <div>
+      <div>test1</div>
+      <Button>Обновить</Button>
+    </div>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
-import * as React from "react";
+import React from "react";
 import {createRoot} from "react-dom/client";
 import "./style.css";
+import App from "./App";
 
 const root = createRoot(document.querySelector('.root'));
-root.render(<h1>Hello, world!</h1>);
+root.render(<App />);
