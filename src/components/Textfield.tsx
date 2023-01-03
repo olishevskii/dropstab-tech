084f28a807be873cf4156f467836108164d16876
Olishevsky Alex <olishevskyalex@gmail.com>
2023-01-04 02:36:33 +0400

    Add Textfield component

---
+++ src/components/Textfield.tsx
@@ -0,0 +1,13 @@
+import React, {InputHTMLAttributes} from "react";
+import clsx from "clsx";
+
+import classes from "./Textfield.css";
+import {CustomFC} from "../types/CustomFC";
+
+interface TextfieldProps extends InputHTMLAttributes<{}> {}
+
+const Textfield: CustomFC<TextfieldProps> = ({className, ...rest}) => (
+  <input className={clsx(className, classes.textfield)} {...rest} />
+)
+
+export default Textfield;
