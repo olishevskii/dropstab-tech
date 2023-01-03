6383b16b6c17e6ab106a08d3940fd6be4e68b9e7
Olishevsky Alex <olishevskyalex@gmail.com>
2023-01-03 21:55:27 +0400

    Fix useRedirect hook

--- src/hooks/useRedirect.tsx
+++ src/hooks/useRedirect.tsx
@@ -1,10 +1,14 @@
 import {useNavigate} from "react-router-dom";
+import {useEffect} from "react";
 
 const useRedirect = (condition: boolean, path: string) => {
   const navigate = useNavigate();
-  if (condition) {
-    navigate(path);
-  }
+
+  useEffect(() => {
+    if (condition) {
+      navigate(path);
+    }
+  }, [condition]);
 }
 
 export default useRedirect;
