8771dc8127c80bc1ec4c2211227d227255400629
Olishevsky Alex <olishevskyalex@gmail.com>
2023-01-03 20:51:19 +0400

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import React, {FC} from "react";
+import React, {FC, useMemo} from "react";
 
-import classes from "./App.css";
-import GraphToolbar from "./components/GraphToolbar";
+import {QueryClient, QueryClientProvider} from "react-query";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Settings from "./pages/Settings";
 
 const App: FC = () => {
+  const queryClient = useMemo(() => new QueryClient(), []);
+
   return (
-    <div className={classes.app}>
-      <GraphToolbar />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <BrowserRouter>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/settings' element={<Settings />} />
+            </Routes>
+        </BrowserRouter>
+      </div>
+    </QueryClientProvider>
   );
 }
 
