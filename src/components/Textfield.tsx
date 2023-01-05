943b7d10776e077310f31a11775440c5386f6b35
Olishevsky Alex <olishevskyalex@gmail.com>
2023-01-06 02:32:12 +0400

    Improve Textfield component

--- src/components/Textfield.tsx
+++ src/components/Textfield.tsx
@@ -1,10 +1,12 @@
-import React, {InputHTMLAttributes} from "react";
+import React, {DetailedHTMLProps} from "react";
 import clsx from "clsx";
 
 import classes from "./Textfield.css";
 import {CustomFC} from "types/CustomFC";
 
-interface TextfieldProps extends InputHTMLAttributes<{}> {}
+interface TextfieldProps extends DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement>{}
 
 const Textfield: CustomFC<TextfieldProps> = ({className, ...rest}) => (
   <input className={clsx(className, classes.textfield)} {...rest} />
