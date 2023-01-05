1249f5faaf893ea8539cc178cb573fe674dd135d
Olishevsky Alex <olishevskyalex@gmail.com>
2023-01-06 00:00:19 +0400

    Add Link component

---
+++ src/components/Link.tsx
@@ -0,0 +1,15 @@
+import React, {DetailedHTMLProps} from "react";
+import clsx from "clsx";
+
+import classes from "./Link.css";
+import {CustomFC} from "types/CustomFC";
+
+export interface LinkProps extends DetailedHTMLProps<
+  React.AnchorHTMLAttributes<HTMLAnchorElement>,
+  HTMLAnchorElement> {}
+
+const Link: CustomFC<LinkProps> = ({className, children, ...rest}) => (
+  <a className={clsx(className, classes.link)} {...rest}>{children}</a>
+);
+
+export default Link;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,6 @@
 import Button from "./Button";
 import Graph from "./Graph";
+import Link from "./Link";
 import Progress from "./Progress";
 import RadioButton from "./RadioButton";
 import Select from "./Select";
@@ -8,6 +9,7 @@ import Textfield from "./Textfield";
 export {
   Button,
   Graph,
+  Link,
   Progress,
   RadioButton,
   Select,
