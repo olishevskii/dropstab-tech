724ff8d1e5653605273d4212cb0c0c3b3f47ab01
Olishevsky Alex <olishevskyalex@gmail.com>
2023-01-06 02:32:18 +0400

    Improve Progress component

--- src/components/Progress.tsx
+++ src/components/Progress.tsx
@@ -1,4 +1,4 @@
-import React, {HTMLAttributes} from "react";
+import React, {DetailedHTMLProps} from "react";
 import clsx from "clsx";
 
 import classes from "./Progress.css";
@@ -9,17 +9,22 @@ export enum ProgressColor {
   SECONDARY = "secondary",
 }
 
-export interface ProgressProps extends HTMLAttributes<HTMLDivElement> {
+export interface ProgressProps extends DetailedHTMLProps<
+  React.HTMLAttributes<HTMLDivElement>,
+  HTMLDivElement>{
   color?: "regular" | "secondary";
 }
 
-const Progress: CustomFC<ProgressProps> = ({className, color = ProgressColor.REGULAR}) => {
+const Progress: CustomFC<ProgressProps> = ({ className,
+                                             color = ProgressColor.REGULAR,
+                                             ...rest}) => {
   const isSecondaryColor = color === ProgressColor.SECONDARY;
   return <div className={clsx(
     className,
     classes.progress,
     isSecondaryColor && classes.secondary
-  )} />;
+  )}
+  {...rest} />;
 }
 
 export default Progress;
