adec487408dfeed5835a5dde03dfe3de6d4723ed
Olishevsky Alex <olishevskyalex@gmail.com>
2023-01-06 02:31:48 +0400

    Improve Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,12 @@
-import React, {ButtonHTMLAttributes} from "react";
+import React, {DetailedHTMLProps} from "react";
 import clsx from "clsx";
 
 import classes from "./Button.css";
 import {CustomFC} from "types/CustomFC";
 
-export interface ButtonProps extends ButtonHTMLAttributes<{}>{}
+export interface ButtonProps extends DetailedHTMLProps<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement>{}
 
 const Button: CustomFC<ButtonProps> = ({className, children, ...rest}) => (
   <button className={clsx(className, classes.button)} {...rest}>{children}</button>
